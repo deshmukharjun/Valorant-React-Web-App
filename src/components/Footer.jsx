49eed602f99707c29b3b8416819a7042fcedd079
Arjun <arjun_deshmukh_aiml@moderncoe.edu.in>
2025-01-29 18:47:11 +0530

    Added Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  { href: "https://discord.com/valorant", icon: <FaDiscord /> },
+  { href: "https://twitter.com/valorant", icon: <FaTwitter /> },
+  { href: "https://www.youtube.com/@valorant", icon: <FaYoutube /> },
 ];
 
 const Footer = () => {
@@ -12,7 +11,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          Valorant© 2025. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
